5b35562c51339f8aa669ef6b5e1587e6b831cece
alelarge <armelle.lelarge@shodo.io>
2023-11-07 21:15:08 +0100

    Init a grid of 3x3 in the test; test failed no compile

--- src/test/kotlin/GameOfLIfeTest.kt
+++ src/test/kotlin/GameOfLIfeTest.kt
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test
 class GameOfLIfeTest {
     @Test
     fun `initialized a 3x3 grid`(){
+        val grid = Grid(3,3)
         assertThat(grid.hasNext()).isTrue
     }
 }
