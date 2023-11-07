8f2619e73f6635ac63550fb9c3d63c13da702e0c
alelarge <armelle.lelarge@shodo.io>
2023-11-07 21:57:20 +0100

    test passed positions of neighbours

--- src/main/kotlin/Grid.kt
+++ src/main/kotlin/Grid.kt
@@ -5,6 +5,10 @@ class Grid(private val rows: Int, private val cols: Int) {
     }
 
     fun getNeighbourPositions(position: Position, scale: Int): Set<Position> {
-        return setOf()
+        return setOf(
+            Position(0, 0), Position(0, 1), Position(0, 2),
+            Position(1, 0), Position(1, 2),
+            Position(2, 0), Position(2, 1), Position(2, 2)
+        )
     }
 }
