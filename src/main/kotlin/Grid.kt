a148df05845c533f6b32d1813534dd670394cb13
alelarge <armelle.lelarge@shodo.io>
2023-11-07 21:42:04 +0100

    test failed must return all positions of the neighbours of the cell

--- src/main/kotlin/Grid.kt
+++ src/main/kotlin/Grid.kt
@@ -1,5 +1,10 @@
+data class Position(var x: Int, var y: Int)
 class Grid(private val rows: Int, private val cols: Int) {
     fun hasNext() : Boolean {
         return true
+    }
+
+    fun getNeighbourPositions(position: Position, scale: Int): Set<Position> {
+        return setOf()
     }
 }
--- src/test/kotlin/GameOfLIfeTest.kt
+++ src/test/kotlin/GameOfLIfeTest.kt
@@ -7,4 +7,20 @@ class GridTest {
         val grid = Grid(3,3)
         assertThat(grid.hasNext()).isTrue
     }
+
+    @Test
+    fun `must return all positions of the neighbours of the cell`() {
+        // Given
+        val grid = Grid(3, 3)
+        val cellNeighbourPositions = grid.getNeighbourPositions(Position(1, 1), 1)
+
+        // When
+        val expectedNeighbours = setOf(
+            Position(0, 0), Position(0, 1), Position(0, 2),
+            Position(1, 0), Position(1, 2),
+            Position(2, 0), Position(2, 1), Position(2, 2)
+        )
+        // Then
+        assertThat(expectedNeighbours.toSet()).isEqualTo(cellNeighbourPositions.toSet())
+    }
 }
