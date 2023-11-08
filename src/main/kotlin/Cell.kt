7f1fd453b80fa7b7c50f0fd675b74e4fc659ca0d
alelarge <armelle.lelarge@shodo.io>
2023-11-08 14:31:28 +0100

    refactor implementation of the fucntion evolve

--- src/main/kotlin/Cell.kt
+++ src/main/kotlin/Cell.kt
@@ -1,16 +1,28 @@
 class Cell (private var cellState: CellState) {
     private var state = cellState
 
-    fun isAlive() : Boolean {
+    fun isAlive(): Boolean {
         return state == CellState.ALIVE
     }
 
-    fun evolve(neighbours: List<Cell>){
-       val liveNeighbours = neighbours.count {it.state == CellState.ALIVE}
-        if (liveNeighbours < 2 || liveNeighbours >3) {
-            state = CellState.DEAD
-        } else if(liveNeighbours == 3) {
-            state = CellState.ALIVE
+    fun evolve(neighbours: List<Cell>) {
+        val liveNeighbours = neighbours.count { it.state == CellState.ALIVE }
+        state = when (state) {
+            CellState.ALIVE -> {
+                if (liveNeighbours < 2 || liveNeighbours > 3) {
+                    CellState.DEAD
+                } else {
+                    CellState.ALIVE
+                }
+            }
+
+            CellState.DEAD -> {
+                if (liveNeighbours == 3) {
+                    CellState.ALIVE
+                } else {
+                    CellState.DEAD
+                }
+            }
         }
     }
 }
