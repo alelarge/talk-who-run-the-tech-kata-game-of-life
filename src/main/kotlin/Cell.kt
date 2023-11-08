bc0df51c872caa53ea71bd88f5fcca950c048780
alelarge <armelle.lelarge@shodo.io>
2023-11-08 09:44:51 +0100

    refactor cell should be dead when initialized as dead

--- src/main/kotlin/Cell.kt
+++ src/main/kotlin/Cell.kt
@@ -2,10 +2,6 @@ class Cell (private var cellState: CellState) {
     private var state = cellState
 
     fun isAlive() : Boolean {
-        if(state == CellState.ALIVE) {
-            return true
-        } else {
-            return false
-        }
+        return state == CellState.ALIVE
     }
 }
