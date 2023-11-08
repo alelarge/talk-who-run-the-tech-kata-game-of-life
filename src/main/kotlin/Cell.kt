511c6a7c028e29572b22cca6308107b89428fb2d
alelarge <armelle.lelarge@shodo.io>
2023-11-08 09:43:24 +0100

    test passed cell should be dead when initialized as dead

--- src/main/kotlin/Cell.kt
+++ src/main/kotlin/Cell.kt
@@ -1,5 +1,11 @@
 class Cell (private var cellState: CellState) {
+    private var state = cellState
+
     fun isAlive() : Boolean {
-        return true
+        if(state == CellState.ALIVE) {
+            return true
+        } else {
+            return false
+        }
     }
 }
