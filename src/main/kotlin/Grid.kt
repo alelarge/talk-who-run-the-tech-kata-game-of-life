3d42eb3d41155dc28fcb5fac576074777395f3d5
alelarge <armelle.lelarge@shodo.io>
2023-11-08 03:15:01 +0100

    test passed return neighbours positions of the cell at bottom right

--- src/main/kotlin/Grid.kt
+++ src/main/kotlin/Grid.kt
@@ -4,10 +4,18 @@ class Grid(private val rows: Int, private val cols: Int) {
     }
 
     fun getNeighbourPositions(position: Position, scale: Int): Set<Position> {
-        return setOf(
-            Position(0, 0), Position(0, 1), Position(0, 2),
-            Position(1, 0), Position(1, 2),
-            Position(2, 0), Position(2, 1), Position(2, 2)
-        )
+        val neighbourPositions = mutableSetOf<Position>()
+
+        for (i in -1 * scale..scale) {
+            for (j in -1 * scale..scale) {
+                if (i == 0 && j == 0) continue
+                val newX = position.x + i
+                val newY = position.y + j
+                if (newX in 0 until rows && newY in 0 until cols) {
+                    neighbourPositions.add(Position(newX, newY))
+                }
+            }
+        }
+        return neighbourPositions
     }
 }
