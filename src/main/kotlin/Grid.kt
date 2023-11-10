a53f1c572305a7d8eca62b17b6e723a0db6494f7
alelarge <armelle.lelarge@shodo.io>
2023-11-10 17:45:42 +0100

    test passes neighbour position

--- src/main/kotlin/Grid.kt
+++ src/main/kotlin/Grid.kt
@@ -1,34 +1,17 @@
 class Grid( val rows: Int, val cols: Int) {
-    private var currentPosition = Position(0, 0)
-    private var isInitialized = false
-    fun hasNext() : Boolean {
-        return currentPosition.x < rows && currentPosition.y < cols
-    }
-    fun next(): Position {
-        if (!isInitialized) {
-            isInitialized = true
-        }
-
-        val position = currentPosition.copy()
-
-        currentPosition.y++
-        if (currentPosition.y == cols) {
-            currentPosition.y = 0
-            currentPosition.x++
-        }
-        return position
-    }
     fun getNeighbourPositions(position: Position, scale: Int): Set<Position> {
-        return setOf(-scale to -scale, 0 to -scale, scale to -scale, -scale to 0, scale to 0, -scale to scale, 0 to scale, scale to scale)
-            .map { (dx, dy) ->
-                val (x, y) = position
-                Position(position.x + dx, position.y + dy)
-            }
-            .filter { (x,y) -> x>=0 && y>=0 && x<rows && y<cols}.toSet()
-    }
 
-    fun reset() {
-        currentPosition = Position(0, 0)
-        isInitialized = false
+        val neighbourPositions = mutableSetOf<Position>()
+                        for (i in -1 * scale..scale) {
+                            for (j in -1 * scale..scale) {
+                                if (i == 0 && j == 0) continue
+                                val newX = position.x + i
+                                val newY = position.y + j
+                                if (newX in 0 until rows && newY in 0 until cols) {
+                                    neighbourPositions.add(Position(newX, newY))
+                                }
+                            }
+                        }
+                        return neighbourPositions
     }
 }
