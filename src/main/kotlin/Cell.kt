200dedc0e8360d0e3c801b10cd510b1dea0a303c
alelarge <armelle.lelarge@shodo.io>
2023-11-13 22:08:05 +0100

    test passed business

--- src/main/kotlin/Cell.kt
+++ src/main/kotlin/Cell.kt
@@ -1,1 +1,18 @@
-//test-failed-compile-business-rule
+enum class CellState {
+    ALIVE,
+    DEAD
+}
+
+class Cell(cellState: CellState) {
+    private var state = cellState
+    fun isAlive(): Boolean {
+        return state == CellState.ALIVE
+    }
+
+    fun evolve(neighbours: List<Cell>) {
+        val liveNeighbours = neighbours.count { it.state == CellState.ALIVE }
+        if (liveNeighbours < 2) {
+            state = CellState.DEAD
+        }
+    }
+}
