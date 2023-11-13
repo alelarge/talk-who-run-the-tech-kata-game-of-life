68d9504c7ed9fb9dbbc1d51a5beeadbc367aa028
alelarge <armelle.lelarge@shodo.io>
2023-11-13 15:22:54 +0100

    live coding position

--- src/test/kotlin/GridTest.kt
+++ src/test/kotlin/GridTest.kt
@@ -12,12 +12,12 @@ class GridTest {
 
     @Test
     fun `must return all positions of the neighbours of the cell at the center of the grid`() {
-        val grid = Grid(3, 3)
+      /*  val grid = Grid(3, 3)
         val cellNeighbourPositions = grid.getNeighbourPositions(Position(1, 1), 1)
         assertThat(cellNeighbourPositions).isEqualTo(setOf(
             Position(0, 0), Position(0, 1), Position(0, 2),
             Position(1, 0), Position(1, 2),
-            Position(2, 0), Position(2, 1), Position(2, 2)))
+            Position(2, 0), Position(2, 1), Position(2, 2)))*/
         Assertions.fail("todo position")
     }
 
