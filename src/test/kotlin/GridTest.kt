9f53748ad7a8c1032c4a8405a55eca14de1594a3
alelarge <armelle.lelarge@shodo.io>
2023-11-13 15:11:19 +0100

    step-test

--- src/test/kotlin/GridTest.kt
+++ src/test/kotlin/GridTest.kt
@@ -3,10 +3,14 @@ import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.Test
 
 class GridTest {
-    @Test
-    fun `initialized a 3x3 grid`() {
-        val grid = Grid(3, 5)
-        assertThat(grid.rows).isEqualTo(3)
-        assertThat(grid.cols).isEqualTo(5)
-    }
+@Test
+fun `initialized a 3x5 grid`() {
+
+
 }
+
+
+}
+
+//assertions-initialized-grid
+// grid-initialized
